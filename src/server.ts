9b3d000009de4328bc4be14b9b02e5524f36257c
Alok Mishra <162608167+alokmishra1807@users.noreply.github.com>
2025-10-15 14:06:35 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import authRouter from "./routes/auth"
 import { connectDB } from "./utlis/dbs";import dotenv from "dotenv";
 dotenv.config();
-
+const port = process.env.PORT || 8000;
 
 const app = express();
 
@@ -17,6 +17,8 @@ app.get("/", (req, res) => {
   res.send("Welcome to my app!!!!!!!");
 });
 
-app.listen(8000, () => {
-  console.log("Server started on port 8000");
+
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
